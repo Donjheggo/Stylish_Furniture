d440239e651ac07edea4813f1fa6000a87d5f050
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-10-10 03:51:32 +0800

    fix orders page

--- app/(tabs)/orders.tsx
+++ app/(tabs)/orders.tsx
@@ -2,25 +2,30 @@ import { View, SafeAreaView, ScrollView } from "react-native";
 import { Text } from "~/components/ui/text";
 import OrderCard from "~/components/orders/order-card";
 import { GetMyOrders } from "~/lib/actions/orders";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useAuth } from "~/context/auth-context";
 import { Tables } from "~/database.types";
+import { useFocusEffect } from "expo-router";
+import { useCallback } from "react";
 
 export default function Screen() {
   const { user } = useAuth();
   const [data, setData] = useState<OrdersT[]>([]);
 
-  useEffect(() => {
-    const fetchOrders = async () => {
-      const data = await GetMyOrders(user?.id || "");
-      if (!data) {
-        return;
-      }
-      setData(data);
-    };
+  useFocusEffect(
+    useCallback(() => {
+      const fetchOrders = async () => {
+        const data = await GetMyOrders(user?.id || "");
+        if (!data) {
+          return;
+        }
+        setData(data);
+      };
 
-    fetchOrders();
-  }, []);
+      fetchOrders();
+    }, [])
+  );
+
 
   return (
     <SafeAreaView className="h-full">
